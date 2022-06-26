58af66f03fc9e48ae75fed9a442c53f9ae840bd3
Alex Tasciyan <erozionnarts@gmail.com>
2022-06-26 17:59:04 -0400

    transition to skia-canvas

--- utils/nowPlayingCanvas.js
+++ utils/nowPlayingCanvas.js
@@ -19,7 +19,7 @@ const splitAtClosestSpace = (str, charsPerLine) => {
 
 const renderMultiLineTitle = (canvas, str, options = {}) => {
   const canvas2D = canvas.getContext('2d')
-  canvas2D.font = options.font || '28px Montserrat-Regular'
+  canvas2D.font = options.font || '28px Montserrat'
   canvas2D.fillStyle = options.fillStyle || '#ffffff'
   canvas2D.textAlign = options.textAlign || 'center'
   const y = options.y || 30
@@ -29,12 +29,15 @@ const renderMultiLineTitle = (canvas, str, options = {}) => {
 
   let textBoxWidth = 0
 
+  // measure the line width then set textBoxWidth to the highest value of the line widths
   for (let i = 0; i < multiLineArray.length; i++) {
-    // measure the line width then set textBoxWidth to the highest value of the line widths
     const lineWidth = canvas2D.measureText(multiLineArray[i]).width
     textBoxWidth = lineWidth > textBoxWidth ? lineWidth : textBoxWidth
   }
+
   const xCentered = (300 - textBoxWidth) / 2 + textBoxWidth / 2
+
+  // render the text
   for (let i = 0; i < multiLineArray.length; i++) {
     canvas2D.fillText(multiLineArray[i], xCentered, y + i * lineHeight)
   }
@@ -53,29 +56,20 @@ const generateNowPlayingCanvas = async (songs) => {
 
   const thumbnail = await loadImage(`https://img.youtube.com/vi/${song.id}/mqdefault.jpg`)
 
-  // imageCtx.drawImage(thumbnail, 0, 0, 222, 125)
+  // Get average thumbnail color
   imageCtx.drawImage(thumbnail, 0, 0, 222, 125)
   const imageBuffer = await imageCanvas.toBuffer()
   const averageColor = await getAverageColor(imageBuffer, {
     defaultColor: [0, 0, 0, 0],
     ignoredColor: [0, 0, 0, 255],
   })
+  const [r, g, b] = averageColor.value
 
   // Generate gradient background
   const boxGradient = canv.createLinearGradient(0, 0, 700, 0)
-  boxGradient.addColorStop(
-    0,
-    `rgba(${averageColor.value[0]}, ${averageColor.value[1]}, ${averageColor.value[2]}, 1)`
-  )
-  boxGradient.addColorStop(
-    0.4,
-    `rgba(${averageColor.value[0]}, ${averageColor.value[1]}, ${averageColor.value[2]}, 1)`
-  )
-  boxGradient.addColorStop(
-    1,
-    `rgba(${averageColor.value[0]}, ${averageColor.value[1]}, ${averageColor.value[2]}, 0)`
-  )
-
+  boxGradient.addColorStop(0, `rgba(${r}, ${g}, ${b}, 1)`)
+  boxGradient.addColorStop(0.4, `rgba(${r}, ${g}, ${b}, 1)`)
+  boxGradient.addColorStop(1, `rgba(${r}, ${g}, ${b}, 0)`)
   canv.fillStyle = boxGradient
   canv.fillRect(300, 0, 400, 435)
 
@@ -87,56 +81,54 @@ const generateNowPlayingCanvas = async (songs) => {
   // Blur thumbnail for background
   canv.filter = 'blur(6px)'
   canv.drawImage(thumbnail, 100, 0, 100, 145, -10, 0, 300, 435)
-
   canv.filter = 'none'
 
-  canv.fillStyle = 'rgba(65, 65, 65, 0.85)' // `rgba(${averageColor.value[0]}, ${averageColor.value[1]}, ${averageColor.value[2]}, 0.75)`
+  // Darken blurred thumbnail
+  canv.fillStyle = 'rgba(65, 65, 65, 0.85)'
   canv.fillRect(0, 0, 300, 435)
 
   canv.drawImage(thumbnail, 0, 0, 300, 169)
 
+  // Split artist and title
   const songInfo = song.name.split(' (')[0].split(/\s*-+\s*/)
+  const [artist, title] = songInfo
 
-  const songTitleHeight = renderMultiLineTitle(canvas, songInfo[1] ? songInfo[1] : songInfo[0], {
-    fillStyle: averageColor.isDark ? '#ffffff' : '#111111',
+  // Render title
+  const songTitleHeight = renderMultiLineTitle(canvas, title || artist, {
+    fillStyle: '#ffffff',
     y: 220,
     x: 0,
-    font: 'bold 24px Montserrat-SemiBold',
+    font: 'bold 24px Montserrat',
   })
 
-  const songArtistHeight = renderMultiLineTitle(canvas, songInfo[0], {
-    fillStyle: averageColor.isDark ? '#ffffff' : '#111111',
-    y: 220 + songTitleHeight,
-    x: 0,
-    font: 'bold 24px Montserrat-Regular',
-  })
-
-  // if (songInfo.length > 1) {
-  //   canv.fillStyle = averageColor.isDark ? '#ffffff' : '#111111'
-  //   canv.font = 'bold 32px Montserrat-SemiBold'
-  //   canv.fillText(`${songInfo[1]}`, 247, 40)
-
-  //   canv.fillStyle = averageColor.isDark ? '#ffffff' : '#111111'
-  //   canv.font = '28px Montserrat-Regular'
-  //   canv.fillText(`${songInfo[0]}`, 247, 70)
-  // } else {
-  //   canv.fillStyle = averageColor.isDark ? '#ffffff' : '#111111'
-  //   canv.font = 'bold 28px Montserrat-SemiBold'
-  //   canv.fillText(`${songInfo[0]}`, 247, 40)
-  // }
-
-  if (songs.length > 1) {
-    canv.font = 'bold 16px Montserrat-Regular'
-    canv.fillText('Up Next:', 247, 100)
-    canv.font = 'bold 22px Montserrat-SemiBold'
-    canv.fillText(`${songs[1].name.split(' (')[0]}`, 247, 120)
+  // Render artist
+  if (title) {
+    renderMultiLineTitle(canvas, artist, {
+      fillStyle: '#ffffff',
+      y: 220 + songTitleHeight,
+      x: 0,
+      font: '24px Montserrat',
+    })
   }
+
+  // Render "Up Next"
+  canv.textAlign = 'left'
+  canv.font = 'bold 28px Montserrat'
+  canv.fillText('Up Next:', 330, 50)
+
+  for (let i = 1; i < Math.min(songs.length, 10); i++) {
+    canv.font = '22px Montserrat'
+    const [a, s] = songs[i].name.split(/\s*(\(|\[)+\s*/)[0].split(/\s*-+\s*/) // [artist, song]
+    canv.fillText(`${i}. ${a}${` - ${s}` || ''}`, 330, 50 + 40 * i)
+  }
+
+  // Buffer canvas
   const buffer = await canvas.toBuffer()
 
   // fs.writeFileSync('./public/test.png', imageBuffer)
   fs.writeFileSync('./public/musicplayer.png', buffer)
 
-  return canvas.toBuffer()
+  return buffer
 }
 
 export { generateNowPlayingCanvas, getAverageColor }
